f182282e111dd02447ecac8c85c96ada21fb2dd7
Luis Fernando Hernandez <hluisfernando1@gmail.com>
2021-05-18 02:29:00 -0500

    feacture init

--- README.md
+++ README.md
@@ -1,27 +1,23 @@
-# AngularTest
+# Slash profile Frontend-Angular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.4.
 
-## Development server
+This project contains the visual layer of the scope of the exercise, here you as a user register your profile or update it and through the google maps api obtain your coordinates
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
+## Build Project
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+`npm install`
+## Run Project
 
-## Build
+`ng serve`
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
 
-## Running unit tests
+  
+# visual presentation
+<img src="./Screenshot.png" alt=""/>
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
-## Running end-to-end tests
 
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
-## Further help
 
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+you could run the frontend side in the url `http://localhost:4200/`
--- src/app/shared/profile/profile.component.ts
+++ src/app/shared/profile/profile.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { ProfileService } from '../../api/profile.service';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers: [ProfileService]
 })
 export class ProfileComponent implements OnInit {
-  
+  edit = false
+  id = ''  
 
   profileForm = new FormGroup({
     nameUser: new FormControl('', [Validators.required]),
@@ -18,21 +22,64 @@ export class ProfileComponent implements OnInit {
   });
 
 
-  constructor() { }
+  constructor(
+    private profileService: ProfileService
+  ) { }
 
   ngOnInit(): void {
+
   }
 
+
+
   public get_exist_profile(){
+    this.edit = false
     if(this.profileForm.get('email').valid){
-      console.log('conectar con servicio que devuelve si existe o no el perfil');
+      this.profileService.consultarProfile(this.profileForm.get('email').value).subscribe(
+        profile => {
+          this.id = profile.data._id
+          this.profileForm.get('nameUser').setValue(profile.data.nameUser);
+          this.profileForm.get('email').setValue(profile.data.email);
+          this.profileForm.get('gender').setValue(profile.data.gender);
+          this.profileForm.get('bio').setValue(profile.data.bio);
+          this.edit = true
+        },
+        error => {
+          console.log(error)
+        }
+      );
     }
   }
 
 
   public submit(){
-    console.log(this.profileForm.value);
+    if(this.edit){
+      this.profileService.updateProfile(this.profileForm.value, this.id).subscribe(
+        profileResponse => {
+          this.profileForm.reset()
+          alert('Perfil actualizado')
+        },
+        error => {
+          console.log(error)
+        }
+      );
+    } else {
+      this.profileService.createProfile(this.profileForm.value).subscribe(
+        profileResponse => {
+          this.profileForm.reset()
+          alert('Perfil creado')
+        },
+        error => {
+          console.log(error)
+        }
+      );
+    }
+    // console.log(this.profileForm.value);
   }
+
+
+
+ 
 
 
 
